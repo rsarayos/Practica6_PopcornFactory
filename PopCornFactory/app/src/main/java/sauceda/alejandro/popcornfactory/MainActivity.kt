3f0b7fc4a1f5f9301e4c1b93a47cf4524ec4077b
rsarayos <raul.sauceda@potros.itson.edu.mx>
2025-02-14 00:56:15 -0700

    Ajustes
    se añaden las series y formato

--- PopCornFactory/app/src/main/java/sauceda/alejandro/popcornfactory/MainActivity.kt
+++ PopCornFactory/app/src/main/java/sauceda/alejandro/popcornfactory/MainActivity.kt
@@ -17,7 +17,9 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     var adapter: PeliculaAdapter? = null
+    var adapter2: PeliculaAdapter? = null
     var peliculas = ArrayList<Pelicula>()
+    var series = ArrayList<Pelicula>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,11 +27,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         cargarPeliculas()
+        cargarSeries()
 
         adapter = PeliculaAdapter(this, peliculas)
+        adapter2 = PeliculaAdapter(this, series)
 
-        var gView = findViewById(R.id.gridview) as GridView
+        var gView = findViewById(R.id.gridviewMovies) as GridView
+        var gView2 = findViewById(R.id.gridviewSeries) as GridView
         gView.adapter = adapter
+        gView2.adapter = adapter2
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -73,6 +79,45 @@ class MainActivity : AppCompatActivity() {
                 "could be the perfect crime. But no amount of careful planning or expertise can prepare the\n" +
                 "team for the dangerous enemy that seems to predict their every move. An enemy that only\n" +
                 "Cobb could have seen coming."))
+    }
+
+    fun cargarSeries(){
+        series.add(Pelicula("Dr. House", R.drawable.drhouse, R.drawable.househeader, "The series follows the life of anti-social, pain killer addict, witty and arrogant medical doctor Gregory\n" +
+                "House (Hugh Laurie) with only half a muscle in his right leg. He and his team of medical doctors try\n" +
+                "to cure complex and rare diseases from very ill ordinary people in the United States of America."))
+        series.add(Pelicula("Smallville", R.drawable.smallville, R.drawable.smallvilleheader, "The numerous miraculous rescues by the local wonder boy Clark have aroused suspicions amongst\n" +
+                "colonials of Smallville. Interestingly, the boy has managed to downplay his acts of various heroic\n" +
+                "egresses in the past. They say he&#39;s either too fast or has a penchant for finding trouble. He was\n" +
+                "found by Martha and Jonathan Kent on the day of the Meteor Shower, and subsequently adopted.\n" +
+                "Clark&#39;s friend Lex Luthor, the only heir of Luthorcorp, has been secretly investigating grounds for\n" +
+                "Clark&#39;s outlandish valor. However, on the face of it, Clark just seems a normal boy who&#39;s slightly\n" +
+                "more secretive than usual."))
+        series.add(Pelicula("Dr. Who", R.drawable.drwho, R.drawable.drwhoheader, "Traveling across time and space, the immortal time-lord known as &#39;The Doctor&#39; travels across the\n" +
+                "universe with his many companions and his loyal shape-shifting space-ship: The TARDIS. The\n" +
+                "Doctor faces many threats across many generations: from The Daleks, The Cybermen and his time-\n" +
+                "lord adversary The Master to the sinister Davros, creator of The Daleks."))
+        series.add(Pelicula("Bones", R.drawable.bones, R.drawable.bonesheader, "Dr. Temperance Brennan is a brilliant, but lonely, anthropologist whom is approached by an\n" +
+                "ambitious FBI agent, named Seely Booth, to help the bureau solve a series of unsolved crimes by\n" +
+                "identifying the long-dead bodies of missing persons by their bone structure. But both Agent Booth\n" +
+                "\n" +
+                "and Dr. Brennan and her team come up again a variety of interference from red tape, corruption,\n" +
+                "and local noncooperation."))
+        series.add(Pelicula("Suits", R.drawable.suits, R.drawable.suitsheader, "While running from a drug deal gone bad, brilliant young college dropout Mike Ross slips into a job\n" +
+                "interview with one of New York City&#39;s best legal closers, Harvey Specter. Tired of cookie-cutter law-\n" +
+                "school grads, Harvey takes a gamble by hiring Mike on the spot after recognizing his raw talent and\n" +
+                "photographic memory. Mike and Harvey are a winning team. Although Mike is a genius, he still has\n" +
+                "a lot to learn about law; and while Harvey might seem like an emotionless, cold-blooded shark,\n" +
+                "Mike&#39;s sympathy and concern for their cases and clients will help remind Harvey why he went into\n" +
+                "law in the first place. Mike&#39;s other allies in the office include the firm&#39;s best paralegal Rachel and\n" +
+                "Harvey&#39;s no-nonsense assistant Donna. Proving to be an irrepressible duo and invaluable to the\n" +
+                "practice, Mike and Harvey must keep their secret from everyone including managing partner\n" +
+                "Jessica and Harvey&#39;s archnemesis Louis, who seems intent on making Mike&#39;s life as difficult as\n" +
+                "possible."))
+        series.add(Pelicula("Friends", R.drawable.friends, R.drawable.friendsheader, "Rachel Green, Ross Geller, Monica Geller, Joey Tribbiani, Chandler Bing and Phoebe Buffay are\n" +
+                "six 20 something year-olds, living off of one another in the heart of New York City. Over the course\n" +
+                "of ten years, this average group of buddies goes through massive mayhem, family trouble, past and\n" +
+                "future romances, fights, laughs, tears and surprises as they learn what it really means to be a\n" +
+                "friend."))
     }
 
 }
