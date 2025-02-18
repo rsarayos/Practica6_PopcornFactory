9256c619b125a66ee3aca225c7efbcdef50431dc
rsarayos <raul.sauceda@potros.itson.edu.mx>
2025-02-17 23:25:48 -0700

    SeatChoosing
    avance

--- PopCornFactory/app/src/main/java/sauceda/alejandro/popcornfactory/DetallePelicula.kt
+++ PopCornFactory/app/src/main/java/sauceda/alejandro/popcornfactory/DetallePelicula.kt
@@ -1,5 +1,6 @@
 package sauceda.alejandro.popcornfactory
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,12 +14,26 @@ class DetallePelicula : AppCompatActivity() {
         setContentView(R.layout.activity_detalle_pelicula)
 
         var bundle = intent.extras
+        var ns = 0
+        var id = -1
+        var titulo = ""
 
         if (bundle != null) {
             findViewById<android.widget.ImageView>(R.id.iv_pelicula_imagen).setImageResource(bundle.getInt("header"))
             findViewById<android.widget.TextView>(R.id.tv_nombre_pelicula).setText(bundle.getString("titulo"))
             findViewById<android.widget.TextView>(R.id.tv_pelicula_desc).setText(bundle.getString("sinopsis"))
 
+            ns = bundle.getInt("numberSeats")
+            id = bundle.getInt("pos")
+            titulo = bundle.getString("titulo")!!
+            findViewById<android.widget.TextView>(R.id.seatsLeft).setText("$ns seats available")
+        }
+
+        var btn_comprar = findViewById<android.widget.Button>(R.id.btn_comprar)
+
+        btn_comprar.setOnClickListener{
+            var intento = Intent(this,SeatSelection::class.java)
+            this.startActivity(intento)
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
